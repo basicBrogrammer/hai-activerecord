8ccf6f76044211896ab007f84dfc011838839900
basicbrogrammer <jcward10@gmail.com>
2022-07-05 22:42:14 -0400

    init commit

---
+++ lib/hai/activerecord.rb
@@ -0,0 +1,10 @@
+# frozen_string_literal: true
+
+require_relative "activerecord/version"
+
+module Hai
+  module Activerecord
+    class Error < StandardError; end
+    # Your code goes here...
+  end
+end
